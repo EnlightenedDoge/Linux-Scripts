f056aa90595eb63260f2e0d692681ceb1815bfe3
Amichai <amichaibarhaim@gmail.com>
2021-12-29 21:31:40 +0200

    finailized bluetooth-toggle script

--- bluetooth-toggle/bluetooth_switch.sh
+++ bluetooth-toggle/bluetooth_switch.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
 mode=$(grep -o '#ControllerMode = [^ ,]\+' <<< `grep '#ControllerMode = ' /etc/bluetooth/main.conf`|awk '{print $3}')
-echo "$mode"
+if [ "$2" == "" ]; then
+echo "Current mode: $mode $2"
+fi
+
+if [ "$EUID" != 0 ]; then
+    sudo "$0" "$1" "0"
+    exit $?
+fi
+
+read -r -p "Are you sure? [y/N] " response
+if [ -z "$response" ] || [[ "$response" =~ [^Yy] ]]; then
+	echo "Terminating..."
+	exit 0
+fi
 if [ "$mode" == "bredr" ]; then
 	sed -i 's/#ControllerMode = bredr/#ControllerMode = dual/g' /etc/bluetooth/main.conf
 	echo "switched to dual"
@@ -11,4 +24,4 @@ elif [ "$mode" == "dual" ]; then
 else
 	echo "Error reading file, no 'ControllerMode' was recognised"
 fi
-sudo service bluetooth restart
+service bluetooth restart
