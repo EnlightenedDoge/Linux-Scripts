947946f67f4bb23c097161bbcdd0d0513fa53ebd
Amichai <amichaibarhaim@gmail.com>
2021-12-30 09:50:27 +0200

    add sudo wrapper

--- bluetooth-toggle/bluetooth_switch.sh
+++ bluetooth-toggle/bluetooth_switch.sh
@@ -2,7 +2,13 @@
 
 mode=$(grep -o '#ControllerMode = [^ ,]\+' <<< `grep '#ControllerMode = ' /etc/bluetooth/main.conf`|awk '{print $3}')
 if [ "$2" == "" ]; then
-echo "Current mode: $mode $2"
+	echo "Current mode: $mode"
+	if [ "$mode" == "bredr" ]; then
+		echo "Single channel and microphone are supported"
+	else
+		echo "Dual channels are supported but without microphone"
+	fi
+	
 fi
 
 if [ "$EUID" != 0 ]; then
@@ -17,11 +23,13 @@ if [ -z "$response" ] || [[ "$response" =~ [^Yy] ]]; then
 fi
 if [ "$mode" == "bredr" ]; then
 	sed -i 's/#ControllerMode = bredr/#ControllerMode = dual/g' /etc/bluetooth/main.conf
-	echo "switched to dual"
+	echo "switched to dual: dual channel, no headset microphone"
 elif [ "$mode" == "dual" ]; then
 	sed -i 's/#ControllerMode = dual/#ControllerMode = bredr/g' /etc/bluetooth/main.conf
-	echo "switched to bredr"
+	echo "switched to bredr: single channel"
 else
 	echo "Error reading file, no 'ControllerMode' was recognised"
 fi
 service bluetooth restart
+
+read -n 1 -s -r -p "Press any key to continue"
