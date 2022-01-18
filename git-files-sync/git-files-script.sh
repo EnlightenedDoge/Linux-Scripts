6b6b606c4cf9c7b57024dfe45d1c111786a969ab
Amichai <amichaibarhaim@gmail.com>
2022-01-18 21:45:06 +0200

    Add support for comments in config file. Comment code

--- git-files-sync/git-files-script.sh
+++ git-files-sync/git-files-script.sh
@@ -2,21 +2,32 @@
 src='https://github.com/EnlightenedDoge/linux-configs.git'
 IFS='/' read -ra repo <<< $src
 repo="${repo[-1]}"
+#get name of github repo to use in path
 repo="${repo:0:(-4)}"
 
+#where the git repo will be placed
 dest="$HOME/Documents/"
+#Where is the config file in which the path to 
+#the files/direcotries to sync is stored
 conf=$(pwd)/config/files
+
 cd "$dest"
 if [ ! -d "$repo" ]; then
     git clone $src
 fi
 cd $dest$repo
+
+
 IFS=' '
 while read -a line
 do
+    #support comments and '~'path-sign in config file
     if [ "${line[0]:0:1}" == "~" ]; then
         line[0]="$HOME${line[0]:1}"
+    elif [ "${line[0]:0:1}" == "#" ]; then
+        continue
     fi
+    #optional used for optional folder name to store files into 
     optional="${line[@]:1}"
     if [ ! -z "$optional" ]; then
         mkdir -p "$optional"
@@ -24,22 +35,29 @@ do
     fi
     cp -R "$line" ./"${line[1]}"
     
-    echo "${line}"
-    echo "$optional"
+    #get file name
     IFS='/' read -ra fname <<< "${line[@]}"
     fname="${fname[@]:(-1)}"
-    echo $fname
+
+    #check for existing git repos in copied directories
+    #and delete them
     if [ ! -z "$optional" ];  then
         isgit=`find ./"$optional"/ -name ".git"`
-        echo "$isgit"
         [ ! -z "$isgit" ]&&rm -r "$isgit"
     elif [ -d ./$fname ]; then
         isgit=`find ./$fname/ -name ".git"`
-        echo $fname"\n$isgit"
         [ ! -z "$isgit" ]&&rm -r "$isgit"
     fi
     optional=""
 done < "$conf"
+
+#create file containing all apps in the app list
+ls /usr/share/applications/ > app_window_list.txt
+
+#create files with all packages installed
+pacman -Qqen > pacmanpkglist.txt
+pacman -Qqem > aurpkglist.txt
+
 
 git add .
 git commit -m "sync `date '+%d/%m/%y-%H:%M'`"
