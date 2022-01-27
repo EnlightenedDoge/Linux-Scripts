13b634d7d78677f7502816f841618410e5319f7a
Amichai <amichaibarhaim@gmail.com>
2022-01-27 23:22:51 +0200

    Finalize repo-checkup.sh

--- git-repo-list-checkout/repo-checkup.sh
+++ git-repo-list-checkout/repo-checkup.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
-ls */ .*/
+for i in $(ls -d */); do
+    echo "$i"
+    cd "$i"
+    if [ ! -d .git ]; then
+        cd ../
+        continue
+    fi
+    git fetch origin
+    reslog="$(git diff HEAD)"
+    if [[ "${reslog}" != "" ]] ; then
+        echo "${i:0:(-1)} is not fully synced"
+    fi
+    reslog="$(git log HEAD..origin/master --oneline)"
+    if [[ "${reslog}" != "" ]] ; then
+        echo "${i:0:(-1)} is not up to date with remote"
+    fi
+    cd ../
+done
